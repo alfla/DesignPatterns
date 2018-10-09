5fa8a2e5a989ba043cf2c7cbd3c7abcc6a3ac856
alfla <luiscordobahoyos@hotmail.com>
2018-10-08 22:01:10 -0500

    Breake Interface as simple as we need

---
+++ SolidD/DependencyInverson.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SolidD
+{
+    class DependencyInverson
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ SolidI/InterfaceSegregation.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using static System.Console;
+
+namespace SolidI
+{
+    public class Document
+    {
+
+    }
+
+    public interface IMachine
+    {
+        void Print(Document d);
+        void Scan(Document d);
+        void Fax(Document d);
+    }
+
+    public class MultiFunctionPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            //
+        }
+
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            //
+        }
+    }
+
+    public class OldFashionedPrinter : IMachine
+    {
+        public void Fax(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Print(Document d)
+        {
+            //
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IPrinter
+    {
+        void Print(Document d);
+    }
+
+    public interface IScanner
+    {
+        void Scan(Document d);
+    }
+
+    public class Photocopier : IPrinter, IScanner
+    {
+        public void Print(Document d)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Document d)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IMultiFunctionDevice: IScanner, IPrinter
+    {
+
+    }
+
+    public class MultiFunctionMachine : IMultiFunctionDevice
+    {
+        private IPrinter printer;
+        private IScanner scanner;
+
+        public MultiFunctionMachine(IPrinter printer, IScanner scanner)
+        {
+            this.printer = printer ?? throw new ArgumentNullException(paramName: nameof(printer));
+            this.scanner = scanner ?? throw new ArgumentNullException(paramName: nameof(scanner));
+        }
+
+        public void Print(Document d)
+        {
+            printer.Print(d);
+        }
+
+        public void Scan(Document d)
+        {
+            scanner.Scan(d);
+        }//Decorator
+    }
+    public class InterfaceSegregation
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
