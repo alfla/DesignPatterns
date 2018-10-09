0bcb0dafc390e99b9ed4fec7d35679fc2942c88a
alfla <luiscordobahoyos@hotmail.com>
2018-10-08 21:42:44 -0500

    We used virtual and override to waranty than square has behaivor of rectangle.

---
+++ SolidL/LiskovSubstitution.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using static System.Console;
+
+namespace SolidL
+{
+    public class Rectangle
+    {
+        public virtual int Width { get; set; }
+        public virtual int Height { get; set; }
+
+        public Rectangle()
+        {
+
+        }
+
+        public Rectangle(int width, int height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Width)}: {Width}, {nameof(Height)}: {Height}";
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public override int Width { set { base.Width = base.Height = value; } }
+        public override int Height { set { base.Height = base.Width = value; } }
+
+    }
+    public class LiskovSubstitution
+    {
+        static public int Area(Rectangle r) => r.Width * r.Height;
+
+        static void Main(string[] args)
+        {
+            Rectangle rc = new Rectangle(2, 3);
+            WriteLine($"{rc} has area {Area(rc)}");
+
+            Square square = new Square();
+            square.Width = 4;
+            WriteLine($"{square} has area {Area(square)}");
+
+            Rectangle rectangle = new Square();
+            rectangle.Width = 4;
+            WriteLine($"{rectangle} has area {Area(rectangle)}");
+            ReadKey();
+
+        }
+    }
+}
