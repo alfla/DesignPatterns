0d2d600e76f78b292e61d076091b14111b841417
alfla <luiscordobahoyos@hotmail.com>
2018-10-15 08:58:45 -0500

    Factory method example

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -12,30 +12,44 @@ namespace FactoryPattern
 
         private double x, y;
 
-        protected Point(double a, double b,
-            CoordinateSystem system = CoordinateSystem.Cartesian)
+        public static Point NewCartesianPoint(double x, double y)
         {
-            switch (system)
-            {
-                case CoordinateSystem.Cartesian:
-                    this.x = a;
-                    this.y = b;
-                    break;
-                case CoordinateSystem.Polar:
-                    this.x = a * Math.Cos(this.y);
-                    this.y = a * Math.Cos(this.y);
-                    break;
-                default:
-                    throw new ArgumentOutOfRangeException(nameof(system), system, null);
-            }
+            return new Point(x, y);
         }
+        public static Point NewPolarPoint(double rho, double theta)
+        {
+            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+        }
+
+        private Point(double x, double y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+
+        public override string ToString()
+        {
+            return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+
     }
 
     class Program
     {
         static void Main(string[] args)
         {
+            Point pointCartesian = Point.NewCartesianPoint(1.0, Math.PI /2);
+            Point pointPolar = Point.NewPolarPoint(1.0, Math.PI / 2);
+
+            Console.WriteLine($"{nameof(pointCartesian)}: {pointCartesian}");
+            Console.WriteLine($"{nameof(pointPolar)}: {pointPolar}");
+            Console.WriteLine(pointCartesian);
+
+
+
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
