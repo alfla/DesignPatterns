d3d0c477b1d9dd93d9c1ebf106a77f0d0f2a2eab
alfla <luiscordobahoyos@hotmail.com>
2018-10-15 09:52:40 -0500

    Inner Factory

--- FactoryPattern/Program.cs
+++ FactoryPattern/Program.cs
@@ -2,37 +2,32 @@
 
 namespace FactoryPattern
 {
-    public static class PointFactory
-    {
-        public static Point NewCartesianPoint(double x, double y)
-        {
-            return new Point(x, y);
-        }
-        public static Point NewPolarPoint(double rho, double theta)
-        {
-            return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
-        }
-
-    }
 
     public class Point
     {
-        public enum CoordinateSystem
-        {
-            Cartesian,
-            Polar
-        }
         private double x, y;
-        public Point(double x, double y)
+        private Point(double x, double y)
         {
             this.x = x;
             this.y = y;
         }
-
-
+        public static Point Origin = new Point(0, 0);
+        public static Point Origin2 => new Point(0,0);
         public override string ToString()
         {
             return $"{nameof(x)}: {x}, {nameof(y)}: {y}";
+        }
+
+        public static class Factory
+        {
+            public static Point NewCartesianPoint(double x, double y)
+            {
+                return new Point(x, y);
+            }
+            public static Point NewPolarPoint(double rho, double theta)
+            {
+                return new Point(rho * Math.Cos(theta), rho * Math.Sin(theta));
+            }
         }
 
     }
@@ -41,15 +36,16 @@ namespace FactoryPattern
     {
         static void Main(string[] args)
         {
-            Point pointCartesian = PointFactory.NewCartesianPoint(1.0, Math.PI /2);
-            Point pointPolar = PointFactory.NewPolarPoint(1.0, Math.PI / 2);
+            Point pointCartesian = Point.Factory.NewCartesianPoint(1.0, Math.PI /2);
+            Point pointPolar = Point.Factory.NewPolarPoint(1.0, Math.PI / 2);
 
             Console.WriteLine($"{nameof(pointCartesian)}: {pointCartesian}");
             Console.WriteLine($"{nameof(pointPolar)}: {pointPolar}");
             Console.WriteLine(pointCartesian);
 
 
-
+            var origin = Point.Origin;
+            Console.WriteLine($"{nameof(origin)}: {origin}");
             Console.WriteLine("Hello World!");
             Console.ReadKey();
         }
