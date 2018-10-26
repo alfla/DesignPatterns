dff7cdbd533acaf84ec966a858b94ab46d4b6e96
alfla <luiscordobahoyos@hotmail.com>
2018-10-25 21:05:24 -0500

    Prototype Pattern

---
+++ PrototypePattern/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace PrototypePattern
+{
+    public class Person
+    {
+        public string[] Names;
+        public Address Address;
+
+        public Person(string[] names, Address address)
+        {
+            Names = names ?? throw new ArgumentNullException($"paramName: {nameof(names)}");
+            Address = address ?? throw new ArgumentNullException($"paramName: {nameof(address)}");
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)} : {string.Join(" ", Names)}, {nameof(Address)} : {Address}";
+        }
+    }
+
+    public class Address
+    {
+        public string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName ?? throw new ArgumentNullException($"paramName: {nameof(streetName)}");
+            HouseNumber = houseNumber;
+        }
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)} : {StreetName}, {nameof(HouseNumber)} : {HouseNumber}.";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var john = new Person(new[] { "John", "Smith" }, new Address("London Road", 123));
+            Console.WriteLine(john);
+            Console.ReadKey();
+        }
+    }
+}
