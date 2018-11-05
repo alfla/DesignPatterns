78a9e91be607f4176c43b2475d89119662d7cfe2
alfla <luiscordobahoyos@hotmail.com>
2018-11-05 16:33:10 -0500

    Monostate Pattern

---
+++ MonostatePattern/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using static System.Console;
+
+
+namespace MonostatePattern
+{
+    public class CEO
+    {
+        private static string name;
+        private static int age;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+    }
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            var ceo = new CEO() { Name = "Lucho", Age = 35 };
+            WriteLine(ceo.ToString());
+            var ceo2 = new CEO();
+            WriteLine(ceo2.ToString());
+            ceo2.Age = 25;
+            ceo2.Name = "Luis";
+            WriteLine(ceo.ToString());
+            WriteLine(ceo2.ToString());
+            ReadKey();
+        }
+    }
+}
