15baebe5b81f7e90cff05431490855677443a563
alfla <luiscordobahoyos@hotmail.com>
2018-11-05 16:17:44 -0500

    DI Singleton

--- SingletonPattern/Program.cs
+++ SingletonPattern/Program.cs
@@ -25,7 +25,59 @@ namespace SingletonPattern
         }
     }
 
+    public class OrdinaryDatabase : IDatabase
+    {
+        private Dictionary<string, int> capitals;
 
+        public OrdinaryDatabase()
+        {
+            WriteLine("Initializing database");
+            capitals = File.ReadAllLines(
+                Path.Combine(
+                    new FileInfo(typeof(IDatabase).Assembly.Location).DirectoryName,
+                    "capitals.txt"))
+                .Batch(2)
+                .ToDictionary(
+                    list => list.ElementAt(0).Trim(),
+                    list => int.Parse(list.ElementAt(1))
+                );
+        }
+
+        public int GetPopulation(string name)
+        {
+            return capitals[name];
+        }
+    }
+    public class ConfigurableRecordFinder
+    {
+        private IDatabase database;
+
+        public ConfigurableRecordFinder(IDatabase database)
+        {
+            this.database = database ?? throw new ArgumentNullException(paramName: nameof(database));
+        }
+
+        public int GetTotalPopulation(IEnumerable<string> names)
+        {
+            int result = 0;
+            foreach (var name in names)
+                result += database.GetPopulation(name);
+            return result;
+        }
+    }
+
+    public class DummyDatabase : IDatabase
+    {
+        public int GetPopulation(string name)
+        {
+            return new Dictionary<string, int>
+            {
+                ["alpha"] = 1,
+                ["beta"] = 2,
+                ["gamma"] = 3
+            }[name];
+        }
+    }
     public class SingletonDatabase : IDatabase
     {
         private Dictionary<string, int> capitals;
--- UnitTestProject1/TestSingleton.cs
+++ UnitTestProject1/TestSingleton.cs
@@ -1,3 +1,4 @@
+using Autofac;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using SingletonPattern;
 
@@ -25,6 +26,31 @@ namespace UnitTestProject1
             
         }
 
+        [TestMethod]
+        public void ConfigurablePopulationTest()
+        {
+            var rf = new ConfigurableRecordFinder(new DummyDatabase());
+            var names = new[] { "alpha", "gamma" };
+            int tp = rf.GetTotalPopulation(names);
+            Assert.AreEqual(tp, 4);
+        }
+
+        [TestMethod]
+        public void DIPopulationTest()
+        {
+            var cb = new ContainerBuilder();
+            cb.RegisterType<OrdinaryDatabase>()
+                .As<IDatabase>()
+                .SingleInstance();
+            cb.RegisterType<ConfigurableRecordFinder>();
+            using (var c = cb.Build())
+            {
+                var rf = c.Resolve<ConfigurableRecordFinder>();
+                var names = new[] { "Seoul", "Mexico City" };
+                int tp = rf.GetTotalPopulation(names);
+                Assert.AreEqual(tp, (17500000 + 17400000));
+            }
+        }
     }
 
 
