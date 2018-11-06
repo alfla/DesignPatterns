84590f4ff62e0526fd790010315b8288ef7a0e95
alfla <luiscordobahoyos@hotmail.com>
2018-11-05 20:54:59 -0500

    Composite Pattern

---
+++ CompositePattern/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CompositePattern
+{
+    public class GraphicObject
+    {
+        public string Color;
+        public virtual string Name { get; set; } = "Group";
+
+        private Lazy<List<GraphicObject>> children = new Lazy<List<GraphicObject>>();
+        public List<GraphicObject> Children => children.Value;
+
+
+        private void Print(StringBuilder sb, int depth)
+        {
+            sb.Append(new string('*', depth))
+                .Append(string.IsNullOrWhiteSpace(Color) ? string.Empty : $"{Color} ")
+                .AppendLine(Name);
+            foreach (var child in Children)
+            {
+                child.Print(sb, depth + 1);
+            }
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            Print(sb, 0);
+            return sb.ToString();
+        }
+    }
+
+    public class Circle : GraphicObject
+    {
+        public override string Name => "Circle"; 
+    }
+
+    public class Square: GraphicObject
+    {
+        public override string Name => "Square";
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var drawing = new GraphicObject { Name = "My Drawnig" };
+            drawing.Children.Add(new Square { Color = "Red" });
+            drawing.Children.Add(new Circle { Color = "Yellow" });
+            var group = new GraphicObject();
+            group.Children.Add(new Square { Color = "Blue" });
+            group.Children.Add(new Circle { Color = "Blue" });
+            drawing.Children.Add(group);
+            Console.WriteLine(drawing);
+                Console.ReadKey();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
