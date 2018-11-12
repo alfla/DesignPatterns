c91c032955d9c9f7edd7c50aa4035aab2ccb1438
alfla <luiscordobahoyos@hotmail.com>
2018-11-12 11:11:12 -0500

    FlyWetight pattern. Nota. encontrar una forma de usar dotmemory.

---
+++ FlyweightPattern/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows;
+using JetBrains.dotMemoryUnit;
+using NUnit.Framework;
+using static System.Console;
+namespace FlyweightPattern
+{
+    public class User
+    {
+        private string FullName;
+
+        public User(string fullName)
+        {
+            this.FullName = fullName;
+        }
+    }
+
+    [TestFixture]
+    class Program
+    {
+        
+        
+
+        [Test]
+        public void TestUser()
+        {
+            var firstNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+            var lastNames = Enumerable.Range(0, 100).Select(_ => RandomString());
+
+            var users = new List<User>();
+            foreach (var firstName in firstNames)
+                foreach (var lastName in lastNames)
+                    users.Add(new User($"{firstName} {lastName}"));
+            ForceGC();
+            dotMemory.Check(memory =>
+            {
+                WriteLine(memory.SizeInBytes);
+                
+            });
+        }
+
+        private void ForceGC()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect();
+        }
+
+        private string RandomString()
+        {
+            Random rand = new Random();
+            return new string(
+                Enumerable.Range(0,10)
+                .Select(i => (char)('a' + rand.Next(26)))
+                .ToArray());
+        }
+    }
+}
