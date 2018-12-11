c335182aaad8c8f19a08b1d19345265c21509507
alfla <luiscordobahoyos@hotmail.com>
2018-12-10 20:21:08 -0500

    iterator pattern

---
+++ ArrayBackend/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace ArrayBackend
+{
+    public class Creature : IEnumerable<int>
+    {
+        private int[] stats = new int[3];
+
+
+        private const int strenght = 0;
+        private const int agility = 1;
+        private const int intelligence = 2;
+
+        public int Strength
+        {
+            get => stats[strenght];
+            set => stats[strenght] = value;
+        }
+
+        public int Agility
+        {
+            get => stats[agility];
+            set => stats[agility] = value;
+        }
+
+        public int Intelligence
+        {
+            get => stats[intelligence];
+            set => stats[intelligence] = value;
+        }
+
+        public double AverageStat =>
+            stats.Average();
+
+        public IEnumerator GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        IEnumerator<int> IEnumerable<int>.GetEnumerator()
+        {
+            return stats.AsEnumerable().GetEnumerator();
+        }
+
+        public int this[int index]
+        {
+            get { return stats[index];}
+            set { stats[index] = value; }
+        }
+        //public int Strength { get; set; }
+        //public int Agility { get; set; }
+        //public int Intelligence { get; set; }
+
+        //public double AverageStat
+        //{
+        //    get { return (Strength + Agility + Intelligence) / 3.0; }
+        //}
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ IteratorObject/Program.cs
@@ -0,0 +1,145 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace IteratorObject
+{
+    public class Node<T>
+    {
+        public T value;
+        public Node<T> Left, Right;
+        public Node<T> Parent;
+
+        public Node(T value)
+        {
+            this.value = value;
+        }
+
+        public Node(T value, Node<T> left, Node<T> right) : this(value)
+        {
+            Left = left;
+            Right = right;
+
+            left.Parent = right.Parent = this;
+        }
+    }
+
+    public class InOrderIterator<T>
+    {
+        public Node<T> Current;
+        public readonly Node<T> root;
+        private bool yieldedStart;
+
+
+        public InOrderIterator(Node<T> root)
+        {
+            this.root = root;
+            Current = root;
+            while (Current.Left != null)
+                Current = Current.Left;
+
+            //   1  <- root
+            //  / \
+            // 2   3
+            // ^ Current
+        }
+
+        public bool MoveNext()
+        {
+            if (!yieldedStart)
+            {
+                yieldedStart = true;
+                return true;
+            }
+
+            if(Current.Right != null)
+            {
+                Current = Current.Right;
+                while (Current.Left != null)
+                    Current = Current.Left;
+                return true;
+            }
+            else
+            {
+                var p = Current.Parent;
+                while(p != null&& Current == p.Right)
+                {
+                    Current = p;
+                    p = p.Parent;
+                }
+                Current = p;
+                return Current != null;
+            }
+        }
+
+        public void Reset()
+        {
+            Current = root;
+            yieldedStart = false;
+        }
+    }
+
+    public class BinaryTree<T>
+    {
+        private Node<T> root;
+
+        public BinaryTree(Node<T> root)
+        {
+            this.root = root;
+        }
+
+        public IEnumerable<Node<T>> InOrder
+        {
+            get
+            {
+                IEnumerable<Node<T>> Traverse(Node<T> current)
+                {
+                    if(current.Left != null)
+                    {
+                        foreach (var left in Traverse(current.Left))
+                            yield return left;
+                    }
+                    yield return current;
+                    if (current.Right!= null)
+                    {
+                        foreach (var right in Traverse(current.Right))
+                            yield return right;
+                    }
+                }
+
+                foreach (var node in Traverse(root))
+                    yield return node;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //   1
+            //  / \
+            // 2   3
+
+            //in-order: 213
+            //
+
+            var root = new Node<int>(1,
+                new Node<int>(2), new Node<int>(3));
+
+            var it = new InOrderIterator<int>(root);
+            while (it.MoveNext())
+            {
+                Write(it.Current.value);
+                Write(',');
+            }
+            WriteLine();
+
+            var tree = new BinaryTree<int>(root);
+            WriteLine(string.Join(",", 
+                tree.InOrder.Select(x => x.value)));
+
+            ReadKey();
+        }
+    }
+}
