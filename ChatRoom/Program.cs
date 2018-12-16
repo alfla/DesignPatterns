9db0f90f4434a415eb80e7bc219b4d9f74d77038
alfla <luiscordobahoyos@hotmail.com>
2018-12-16 11:04:52 -0500

    mediator pattern

---
+++ ChatRoom/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using static System.Console;
+
+namespace ChatRoom
+{
+    public class Person
+    {
+        public string Name;
+        public RoomChat roomChat;
+        private List<string> chatLog = new List<string>();
+
+        public Person(string name)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+        }
+
+        public void Say(string message)
+        {
+            roomChat.Broadcast(Name, message);
+        }
+
+        public void PrivateMessage(string who, string message)
+        {
+            roomChat.Message(Name, who, message);
+        }
+
+        public void Receive(string sender, string message)
+        {
+            string s = $"{sender}: '{message}'";
+            chatLog.Add(s);
+            WriteLine($"[{Name}' chat session] {s}");
+        }
+    }
+
+    public class RoomChat
+    {
+        private List<Person> people = new List<Person>();
+
+        public void Join(Person p)
+        {
+            string joinMsg = $"{p.Name} joins the chat";
+            Broadcast("room", joinMsg);
+
+            p.roomChat = this;
+            people.Add(p);
+        }
+
+        public void Broadcast(string source, string message)
+        {
+            foreach (var p in people)
+            {
+                if (p.Name != source)
+                    p.Receive(source, message);
+            }
+        }
+
+        public void Message(string source, string destination, string message)
+        {
+            people.FirstOrDefault(p => p.Name == destination)
+                ?.Receive(source, message);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var room = new RoomChat();
+
+            var jhon = new Person("Jhon");
+            var jane = new Person("Jane");
+
+
+            room.Join(jhon);
+            room.Join(jane);
+
+            jhon.Say("Hi");
+            jane.Say("oh, hey jhon");
+
+            var simon = new Person("Simon");
+            room.Join(simon);
+
+            simon.Say("hi everyone");
+            jane.PrivateMessage("Simon", "glad you could join us!");
+
+
+            ReadKey();
+            //Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ EventBroker/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using Timer = System.Timers.Timer;
+using static System.Console;
+using System.Reactive.Subjects;
+using System.Reactive.Linq;
+using Autofac;
+
+namespace EventBroker
+{
+    public class Actor
+    {
+        protected EventBroker broker;
+
+        public Actor(EventBroker broker)
+        {
+            this.broker = broker ?? throw new ArgumentNullException(paramName: nameof(broker));
+        }
+
+    }
+
+    public class FootballPlayer : Actor
+    {
+
+        public string Name { get; set; }
+        public int GoalsScored { get; set; } = 0;
+
+        public void Score()
+        {
+            GoalsScored++;
+            broker.Publish(new PlayerScoredEvent { Name = Name, GoalsSocred = GoalsScored });
+        }
+
+        public void AssaultReferee()
+        {
+            broker.Publish(new PlayerSentOffEvent { Name = Name, Reason = "violence" });
+        }
+
+        public FootballPlayer(EventBroker broker, string name) : base(broker)
+        {
+            Name = name ?? throw new ArgumentNullException(nameof(name));
+
+            broker.OfType<PlayerScoredEvent>()
+                .Where(ps => !ps.Name.Equals(name))
+                .Subscribe(
+                    ps => WriteLine($"{name}: Nicely done, {ps.Name}! It's your {ps.GoalsSocred} goal."));
+
+            broker.OfType<PlayerSentOffEvent>()
+                .Where(ps => !ps.Name.Equals(name))
+                .Subscribe(
+                    ps => WriteLine($"{name}: see you int lockers, {ps.Name}"));
+
+
+        }
+
+    }
+
+    public class FootballCoach : Actor
+    {
+        public FootballCoach(EventBroker broker) : base(broker)
+        {
+
+            broker.OfType<PlayerScoredEvent>()
+                .Subscribe(pe =>
+                {
+                    if (pe.GoalsSocred < 3)
+                        WriteLine($"Coach: well done, {pe.Name}!");
+                });
+
+            broker.OfType<PlayerSentOffEvent>()
+                .Subscribe(pe =>
+                {
+                    if (pe.Reason == "violence")
+                        WriteLine($"Coach: how could you, {pe.Name}.");
+                });
+        }
+    }
+
+    public class PlayerEvent
+    {
+        public string Name { get; set; }
+    }
+
+    public class PlayerScoredEvent : PlayerEvent
+    {
+        public int GoalsSocred { get; set; }
+    }
+
+    public class PlayerSentOffEvent : PlayerEvent
+    {
+        public string Reason { get; set; }
+    }
+
+    public class EventBroker : IObservable<PlayerEvent>
+    {
+        private Subject<PlayerEvent> subscriptions = new Subject<PlayerEvent>();
+
+        public IDisposable Subscribe(IObserver<PlayerEvent> observer)
+        {
+            return subscriptions.Subscribe(observer);
+        }
+
+        public void Publish(PlayerEvent pe)
+        {
+            subscriptions.OnNext(pe);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cb = new ContainerBuilder();
+            cb.RegisterType<EventBroker>().SingleInstance();
+            cb.RegisterType<FootballCoach>().SingleInstance();
+            cb.Register((c, p) =>
+            new FootballPlayer(
+                c.Resolve<EventBroker>(),
+                p.Named<string>("name")));
+
+            using(var c = cb.Build())
+            {
+                var coach = c.Resolve<FootballCoach>();
+                var player1 = c.Resolve<FootballPlayer>(new NamedParameter("name", "John"));
+                var player2 = c.Resolve<FootballPlayer>(new NamedParameter("name", "Chris"));
+
+                player1.Score();
+                player1.Score();
+                player1.Score();
+                player1.AssaultReferee();
+                player2.Score();
+
+
+            }
+            ReadKey();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
