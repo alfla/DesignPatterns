7b481579bf6027dc20293959b5d63ef80732779e
alfla <luiscordobahoyos@hotmail.com>
2019-01-05 15:54:14 -0500

    Method pattern and first shot visitor pattern

---
+++ TemplatePattern/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using static System.Console;
+
+namespace TemplatePattern
+{
+    public abstract class Game
+    {
+        public void Run()
+        {
+            Start();
+            while (!HaveWinner)
+            {
+                TakeTurn();
+            }
+            WriteLine($"Player {WinningPlayer} wins");
+        }
+
+        protected int currentPlayer;
+        protected readonly int numberOfPlayers;
+
+        protected Game(int numberOfPlayers)
+        {
+            this.numberOfPlayers = numberOfPlayers;
+        }
+
+        protected abstract void Start();
+        protected abstract void TakeTurn();
+        protected abstract bool HaveWinner { get; }
+        protected abstract int WinningPlayer { get; }
+    }
+
+
+    public class Chess : Game
+    {
+        public Chess() : base(2)
+        {
+
+        }
+
+        protected override bool HaveWinner => turn == maxTurn;
+
+        protected override int WinningPlayer => currentPlayer;
+
+        protected override void Start()
+        {
+            WriteLine($"Starting a game of chess with {numberOfPlayers} playes.");
+
+        }
+
+        protected override void TakeTurn()
+        {
+            WriteLine($"Turn {turn++} taken by player {currentPlayer}.");
+            currentPlayer = (currentPlayer + 1) % numberOfPlayers;
+        }
+
+        private int turn = 1;
+        private int maxTurn = 10;
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var chess = new Chess();
+            chess.Run();
+            ReadKey();
+            WriteLine("Hello World!");
+        }
+    }
+}
